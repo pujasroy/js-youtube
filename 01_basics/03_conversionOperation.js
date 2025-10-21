2f2aa3219941c7797ee6b9638143ae3382759c43
Puja Sinha Roy <puja159btcse25@igdtuw.ac.in>
2025-10-21 13:59:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+// let score="33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// //-----------//
+
+// let newScore="33abc"
+
+// console.log(typeof newScore);
+// console.log(typeof (newScore));
+
+// let valueInNew=Number(newScore)
+// console.log(typeof valueInNew);
+// console.log(valueInNew); //Not a Number (same output for undefined)
+
+// //-----------//
+
+// let thirdScore=null
+
+// console.log(typeof thirdScore);
+// console.log(typeof (thirdScore));
+
+// let valueInThird=Number(thirdScore)
+// console.log(typeof valueInThird);
+// console.log(valueInThird);
+
+
+// //for boolean values like true and false, we get output as 1 and 0
+// //for string, we get output as NaN
+
+// let isLoggedIn=1 
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=> true; 2=>false
+//""=> false
+//"Puja"=> true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
