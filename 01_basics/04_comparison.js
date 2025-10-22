aa59e3788c716c4e27b72e0d4ac961c026882f7e
Puja Sinha Roy <puja159btcse25@igdtuw.ac.in>
2025-10-22 13:23:14 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1);
+// console.log("02">1);
+//=>JS automatically converts the number inside string to number and then compares
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// =>an equality check and comparisons work differently,
+// comparisons convert null to a number, treating it as zero
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0;)
+// =>all give output as false
+
+//=== checks both datatype and value
