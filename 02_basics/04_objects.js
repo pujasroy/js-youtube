27e34031ef6a94b84d3da5d77d1079c9566d00ce
Puja Sinha Roy <puja159btcse25@igdtuw.ac.in>
2025-10-24 19:03:08 +0000

    edits

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -25,7 +25,7 @@ const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
 const obj3={5:"a",6:"b"}
 
-// const obj3={obj1, obj2} =>objects inside object
+// const obj4={obj1, obj2} =>objects inside object
 
 const obj4=Object.assign({},obj1,obj2,obj3) //{} acts as target and obj1,obj2,obj3 as source
 console.log(obj4);
