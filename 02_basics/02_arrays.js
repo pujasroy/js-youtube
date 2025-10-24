f56cc195316a8d9e66a47dae2cee38a3ebdf60b0
Puja Sinha Roy <puja159btcse25@igdtuw.ac.in>
2025-10-24 07:43:34 +0000

    arrays pt 2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,10 @@
+const marvel_heroes=["thor","ironman","spiderman"]
+const dc_heroes=["superman","flash","batman"]
+
+// marvel_heroes.push(dc_heroes); //adds the second array as an element of the first array
+// console.log(marvel_heroes);
+// console.log(marvel_heroes[3][1]);
+
+// const allHeroes= marvel_heroes.concat(dc_heroes); //adds both arrays
+// console.log(allHeroes);
+
